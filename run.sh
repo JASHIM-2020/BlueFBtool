145c44f3dcb89a046dd3097ce4c0d25d0ce9f1f4
TERMUXID7 <55914808+BLUE-DEVIL1134@users.noreply.github.com>
2020-03-27 23:21:33 +0530

    BASH EXECUTE THIS!!

---
+++ run.sh
@@ -0,0 +1,21 @@
+pkg install python 
+pkg install python2
+pkg install toilet
+pkg install cowsay
+pkg install wget
+pkg install git
+pkg install openssl
+pkg install openssh
+pkg install tor
+pkg install proot
+pkg install fakeroot
+pkg install fish
+clear
+ls
+apt update && apt upgrade 
+clear
+ls
+toilet -f big -F gay "AM-BD"
+python config.py
+
+
